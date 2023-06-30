b6f2b4050e12f98c6e02f359d35220736fdad317
KJH <gh7052@gmail.com>
2023-06-30 19:56:45 +0900

    [REFACTOR] : navigate 로 이동 -> intent 로 이동

--- app/src/main/java/com/kjh/dietmanagement/ui/homedialog/HomeDialogFragment.kt
+++ app/src/main/java/com/kjh/dietmanagement/ui/homedialog/HomeDialogFragment.kt
@@ -1,15 +1,16 @@
 package com.kjh.dietmanagement.ui.homedialog
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.kjh.dietmanagement.R
 import com.kjh.dietmanagement.databinding.FragmentHomeDialogBinding
+import com.kjh.dietmanagement.ui.mealform.MealFormActivity
 
 class HomeDialogFragment : BottomSheetDialogFragment() {
 
@@ -20,20 +21,21 @@ class HomeDialogFragment : BottomSheetDialogFragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_dialog, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        onAddButton()
+        onMoveButton()
         binding.weekDay = args.weekday
     }
 
-    private fun onAddButton() {
+    private fun onMoveButton() {
         binding.btAdd.setOnClickListener {
-            findNavController().navigate(R.id.action_homeDialogFragment_to_mealFormFragment)
+            val intent = Intent(activity, MealFormActivity::class.java)
+            startActivity(intent)
         }
     }
 }
