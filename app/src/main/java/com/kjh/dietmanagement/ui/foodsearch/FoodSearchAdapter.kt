7cb8874b689db6c4ec5bfc62c5f2eb1c21c49a86
KJH <gh7052@gmail.com>
2023-06-30 19:32:51 +0900

    [ADD] : FoodSearchAdapter 추가

---
+++ app/src/main/java/com/kjh/dietmanagement/ui/foodsearch/FoodSearchAdapter.kt
@@ -0,0 +1,34 @@
+package com.kjh.dietmanagement.ui.foodsearch
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kjh.dietmanagement.databinding.ItemFoodsearchBinding
+import com.kjh.dietmanagement.model.Food
+import com.kjh.dietmanagement.ui.common.FoodDiffCallback
+import com.kjh.dietmanagement.ui.common.OnClickInterface
+
+class FoodSearchAdapter(private val onClickListener: OnClickInterface) : ListAdapter<Food, FoodSearchAdapter.ViewHolder>(FoodDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemFoodsearchBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemFoodsearchBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Food) {
+            with(binding) {
+                food = item
+                btDelete.setOnClickListener {
+                    onClickListener.onClick(layoutPosition)
+                }
+                executePendingBindings()
+            }
+        }
+    }
+}
