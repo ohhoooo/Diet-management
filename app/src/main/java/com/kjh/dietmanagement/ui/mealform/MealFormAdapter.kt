810032ea169f5c10310be2b0a365a6c826779452
KJH <gh7052@gmail.com>
2023-07-01 19:39:56 +0900

    [ADD] : MealFormAdapter 추가

---
+++ app/src/main/java/com/kjh/dietmanagement/ui/mealform/MealFormAdapter.kt
@@ -0,0 +1,34 @@
+package com.kjh.dietmanagement.ui.mealform
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kjh.dietmanagement.databinding.ItemMealformBinding
+import com.kjh.dietmanagement.model.Food
+import com.kjh.dietmanagement.ui.common.FoodDiffCallback
+import com.kjh.dietmanagement.ui.common.OnClickInterface
+
+class MealFormAdapter(private val onClickListener: OnClickInterface) : ListAdapter<Food, MealFormAdapter.ViewHolder>(FoodDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemMealformBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    inner class ViewHolder(private val binding: ItemMealformBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Food) {
+            with(binding) {
+                food = item
+                btDelete.setOnClickListener {
+                    onClickListener.onClick(layoutPosition)
+                }
+                executePendingBindings()
+            }
+        }
+    }
+}
