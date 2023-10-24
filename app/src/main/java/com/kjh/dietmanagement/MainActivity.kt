f004af71a9a26a6b57c268d2597f143fa94a313c
KJH <gh7052@gmail.com>
2023-10-25 02:35:03 +0900

    feat: 특정 fragment의 하단 바 숨김 처리

--- app/src/main/java/com/kjh/dietmanagement/MainActivity.kt
+++ app/src/main/java/com/kjh/dietmanagement/MainActivity.kt
@@ -2,7 +2,9 @@ package com.kjh.dietmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.kjh.dietmanagement.databinding.ActivityMainBinding
@@ -15,10 +17,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        // NavController 획득
-        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.findNavController()
-        navController?.let {
-            binding.bottomNavigationView.setupWithNavController(it)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+
+        // BottomNavigationView 에게 NavController 설정
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // 특정 fragment 들을 제외한 나머지 fragment 들의 bottom navigation bar 숨김 처리
+        navController.addOnDestinationChangedListener { _, view, _ ->
+            if (view.id == R.id.homeFragment) {
+                binding.bottomNavigationView.visibility = View.VISIBLE
+            }else {
+                binding.bottomNavigationView.visibility = View.GONE
+            }
         }
     }
 }
