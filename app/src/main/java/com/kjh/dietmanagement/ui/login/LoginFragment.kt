6e36f88bdfa9b3e33084651540883bd696cb56b9
KJH <gh7052@gmail.com>
2023-11-01 23:07:38 +0900

    feat: 로그인 화면 ui 추가

---
+++ app/src/main/java/com/kjh/dietmanagement/ui/login/LoginFragment.kt
@@ -0,0 +1,40 @@
+package com.kjh.dietmanagement.ui.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.kjh.dietmanagement.R
+import com.kjh.dietmanagement.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        onClickButton()
+    }
+
+    private fun onClickButton() {
+        binding.tvLoginButton.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
