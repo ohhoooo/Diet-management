f855c32855879b6f5b27f486a0a6aafa660572bf
KJH <gh7052@gmail.com>
2023-11-07 20:35:56 +0900

    feat: 로그인 기능 추가

---
+++ app/src/main/java/com/kjh/dietmanagement/model/Login.kt
@@ -0,0 +1,19 @@
+package com.kjh.dietmanagement.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Login(
+    @SerializedName("user_id") val userId: String,
+    @SerializedName("password") val password : String
+)
+
+data class ResponseLogin (
+    @SerializedName("code") val code: Int,
+    @SerializedName("isSuccess") val isSuccess: Boolean,
+    @SerializedName("message") val message: String,
+    @SerializedName("result") val result: Result
+)
+
+data class Result (
+    @SerializedName("jwt") val jwt : String
+)
--- app/src/main/java/com/kjh/dietmanagement/ui/login/LoginFragment.kt
+++ app/src/main/java/com/kjh/dietmanagement/ui/login/LoginFragment.kt
@@ -1,14 +1,21 @@
 package com.kjh.dietmanagement.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.kjh.dietmanagement.R
 import com.kjh.dietmanagement.databinding.FragmentLoginBinding
+import com.kjh.dietmanagement.model.Login
+import com.kjh.dietmanagement.model.ResponseLogin
+import com.kjh.dietmanagement.network.ApiClient
+import retrofit2.Call
+import retrofit2.Response
 
 class LoginFragment : Fragment() {
 
@@ -34,8 +41,23 @@ class LoginFragment : Fragment() {
     private fun onClickButton() {
         // 로그인 버튼
         binding.tvLoginButton.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
-            findNavController().navigate(action)
+            ApiClient.create().loginUser(Login(binding.etId.text.toString(), binding.etPassword.text.toString())).enqueue(object : retrofit2.Callback<ResponseLogin> {
+                override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
+                    if (response.isSuccessful) {
+                        Log.d("Response Yes: ", response.body().toString())
+                        Toast.makeText(requireContext(), "로그인 성공 하였습니다.", Toast.LENGTH_SHORT).show()
+                        val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+                        findNavController().navigate(action)
+                    } else {
+                        Log.d("Response NO: ", response.body()?.message.toString())
+                        Toast.makeText(requireContext(), "로그인 실패 하였습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
+                    Log.d("Response onFailure: ", t.localizedMessage)
+                    Toast.makeText(requireContext(), "로그인 실패 하였습니다.", Toast.LENGTH_SHORT).show()
+                }
+            })
         }
 
         // 회원 가입 버튼
